b3c4792feb3a619b8c38e15d316b50b472351a72
SamBarFu <samuelbarberena12@gmail.com>
2026-05-06 15:36:59 -0600

    chore: remove swagger-ui-express and yamljs dependencies

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -5,18 +5,9 @@ import cookieParser from 'cookie-parser';
 import { notFoundMiddleware } from '@middlewares/notFound.ts';
 import { errorHandlerMiddleware } from '@middlewares/errorHandler.ts';
 import v1Router from '@/routes/v1/index.ts';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import { ENV } from '@/core/config/env.ts';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app: Application = express();
-
-const swaggerDocument = YAML.load(path.join(__dirname, './docs/openapi.yaml'));
 
 app.use(helmet());
 app.use(
@@ -39,9 +30,6 @@ app.use(cookieParser());
 app.get('/', (_req, res) => {
   res.json({ message: 'Hola Mundo' });
 });
-
-// Swagger Documentation
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/v1', v1Router);
 
